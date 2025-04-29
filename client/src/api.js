959459bafcbb9e5250fca504e1219fa4abc088bf
NuviSenanya <nuvindyachamudini@gmail.com>
2025-04-29 21:37:59 +0530

    Update Code

--- client/src/api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const API_URL = "http://localhost:5001"; 
-
-export const getExampleData = async () => {
-  const response = await axios.get(`${API_URL}/your-endpoint`);
-  return response.data;
-};
